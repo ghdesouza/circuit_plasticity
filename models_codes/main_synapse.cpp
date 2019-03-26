db54a5466521a82ec371b736198fcab40e0426dc
ghdesouza <ghdesouza@gmail.com>
2019-03-26 15:12:19 -0300

    partial

--- models_codes/main_synapse.cpp
+++ models_codes/main_synapse.cpp
@@ -22,25 +22,19 @@ int main(){
 		syn->step(dt, iz->get_voltage(), izpos->get_voltage(), (iz->get_voltage() > 0));
 		izpos->set_I_inj(-syn->get_possynaptic_current());
 		synback->step(dt, izpos->get_voltage(), iz->get_voltage(), (iz->get_voltage() > 0));
-		if(i*dt >= 0 && i*dt <= 30
-		   //|| i*dt >= 35 && i*dt <= 50
-		   //||i*dt >= 65 && i*dt <= 80
-		   ) iz->set_I_inj(50-synback->get_possynaptic_current());
+		if(i*dt >= 0 && i*dt <= 30) iz->set_I_inj(50-synback->get_possynaptic_current());
 		else iz->set_I_inj(-synback->get_possynaptic_current());
 		
 		iz->step(dt);
 		izpos->step(dt);
 		
-		//if(i*dt >= 9 && i*dt <= 30) fprintf(arq, "%f\t%f\n", i*dt, syn->get_possynaptic_current());
-		//if(i*dt >= 0 && i*dt <= 50) fprintf(arq, "%f\t%f\n", i*dt, izpos->get_voltage());
 		if(i*dt >= 0 && i*dt <= 30) fprintf(arq, "%f\t%f\t%f\t%f\t%f\t%f\t%f\n", i*dt, 
 											iz->get_voltage(), syn->get_T(), syn->get_variable(1),
 											izpos->get_voltage(), synback->get_T(), synback->get_variable(1));
 	}
 	fclose(arq);
 
-	if(system("python3 results/syn/graphic.py \"voltage \\ [mV]\" \"u\" "));
-	//if(system("xdg-open figura.eps"))
+	if(system("python3 results/syn/graphic.py"));
 
 	delete iz;
 	delete izpos;
